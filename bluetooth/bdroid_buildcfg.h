6aa8b82a2955a62804ff73c5674d1ef31e5ee2cf
iBullRay <bullray097@yandex.ru>
2018-08-03 22:25:49 +0300

    msm8226-common: Configs update
    * Update codecs from I9301IXXSAQA1.
    * Bluetooth: use full name instead of device model.

--- bluetooth/bdroid_buildcfg.h
+++ bluetooth/bdroid_buildcfg.h
@@ -17,7 +17,7 @@
 #ifndef _BDROID_BUILDCFG_H
 #define _BDROID_BUILDCFG_H
 
-#define BTM_DEF_LOCAL_NAME "GT-I930XX"
+#define BTM_DEF_LOCAL_NAME "Samsung Galaxy S3 Neo"
 #define BTA_DISABLE_DELAY 1000 /* in milliseconds */
 
 #define BLUETOOTH_QTI_SW TRUE
